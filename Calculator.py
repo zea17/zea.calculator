b646c578ba8ae2bc34b29c862590cf39ebc314a7
zea <93328643+zea17@users.noreply.github.com>
2022-06-20 02:35:29 +0430

    added const variable

--- Calculator.py
+++ Calculator.py
@@ -1,10 +1,35 @@
 import toga
 from toga.style.pack import COLUMN, Pack
 
+BUTTON_WIDTH = 55
+BUTTON_HEIGHT = 45
+BUTTON_PADDING = 6
+
+DISPLAY_HEIGHT = 80
+
+WINDOW_WIDTH = 4*BUTTON_WIDTH + 5*BUTTON_PADDING
+WINDOW_HEIGHT = DISPLAY_HEIGHT + 5*BUTTON_WIDTH + BUTTON_PADDING*6
+
+
+def add_button_to_box(box, is_left_most, text):
+    left_padding = 0
+    if is_left_most:
+        left_padding = BUTTON_PADDING
+    button = toga.Button(text, style=Pack(
+        width=BUTTON_WIDTH,
+        height=BUTTON_HEIGHT,
+        padding=BUTTON_PADDING,
+        padding_left=left_padding,
+    ))
+    box.add(button)
+
 
 def create_main_window_content(app):
     box = toga.Box()
-
+    add_button_to_box(box, True, "⌫")
+    add_button_to_box(box, False, "±")
+    add_button_to_box(box, False, "%")
+    add_button_to_box(box, False, "÷")
     return box
 
 
@@ -14,7 +39,7 @@ class ExampleWindow(toga.App):
 
     def startup(self):
         self.window = toga.Window(
-            size=(270, 380),
+            size=(250, 340),
             title="Zea Calc",
             resizeable=False,
             on_close=self.quit,
