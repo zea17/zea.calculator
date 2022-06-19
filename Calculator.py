1273778a3f97dbf99d3467b5348aba153454e9fd
zea <93328643+zea17@users.noreply.github.com>
2022-06-19 23:42:31 +0430

    template with window

--- Calculator.py
+++ Calculator.py
@@ -2,16 +2,25 @@ import toga
 from toga.style.pack import COLUMN, Pack
 
 
+def create_main_window_content(app):
+    box = toga.Box()
+
+    return box
+
+
 class ExampleWindow(toga.App):
     def quit(self, x):
         self.exit()
 
     def startup(self):
-        self.label = toga.Label("hello world")
         self.window = toga.Window(
-            size=(270, 380), title="Zea Calc", resizeable=False, on_close=self.quit)
+            size=(270, 380),
+            title="Zea Calc",
+            resizeable=False,
+            on_close=self.quit,
+        )
         self.window.app = self
-        self.window.content = self.label
+        self.window.content = create_main_window_content(self)
         self.window.show()
 
 
